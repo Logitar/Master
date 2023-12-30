7dffa72d06447bcf4aaa640367424abb2502b3a8
Francis Pion <francispion@hotmail.com>
2023-12-29 21:37:51 -0500

    Implementing project management.

---
+++ backend/src/Logitar.Master.Infrastructure/Commands/InitializeDatabaseCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Logitar.Master.Infrastructure.Commands;
+
+/// <summary>
+/// The command raised to initialize the database.
+/// </summary>
+public record InitializeDatabaseCommand : INotification;
---
+++ backend/src/Logitar.Master.Infrastructure/DependencyInjectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Logitar.EventSourcing.Infrastructure;
+using Logitar.Master.Application;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logitar.Master.Infrastructure;
+
+/// <summary>
+/// Provides extension methods for dependency injection.
+/// </summary>
+public static class DependencyInjectionExtensions
+{
+  /// <summary>
+  /// Registers infrastructure services to the specified service collection.
+  /// </summary>
+  /// <param name="services">The service collection.</param>
+  /// <returns>The service collection.</returns>
+  public static IServiceCollection AddLogitarMasterInfrastructure(this IServiceCollection services)
+  {
+    return services
+      .AddLogitarEventSourcingInfrastructure()
+      .AddLogitarMasterApplication();
+  }
+}
