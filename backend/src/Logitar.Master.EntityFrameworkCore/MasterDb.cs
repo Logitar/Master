52434569f1509c73faa8fe54eb3925a5c33331d4
Francis Pion <francispion@hotmail.com>
2024-04-26 00:50:49 -0400

    Added Actors table.

--- backend/src/Logitar.Master.EntityFrameworkCore/MasterDb.cs
+++ backend/src/Logitar.Master.EntityFrameworkCore/MasterDb.cs
@@ -5,6 +5,19 @@ namespace Logitar.Master.EntityFrameworkCore;
 
 internal static class MasterDb
 {
+  public static class Actors
+  {
+    public static readonly TableId Table = new(nameof(MasterContext.Actors));
+
+    public static readonly ColumnId ActorId = new(nameof(ActorEntity.ActorId), Table);
+    public static readonly ColumnId DisplayName = new(nameof(ActorEntity.DisplayName), Table);
+    public static readonly ColumnId EmailAddress = new(nameof(ActorEntity.EmailAddress), Table);
+    public static readonly ColumnId Id = new(nameof(ActorEntity.Id), Table);
+    public static readonly ColumnId IsDeleted = new(nameof(ActorEntity.IsDeleted), Table);
+    public static readonly ColumnId PictureUrl = new(nameof(ActorEntity.PictureUrl), Table);
+    public static readonly ColumnId Type = new(nameof(ActorEntity.Type), Table);
+  }
+
   public static class Projects
   {
     public static readonly TableId Table = new(nameof(MasterContext.Projects));
