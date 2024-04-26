d39f2cc0de88b839d7b4cd52ffaff7230d8b641b
Francis Pion <francispion@hotmail.com>
2024-04-25 21:30:28 -0400

    Created a REST API.

---
+++ backend/src/Logitar.Master/Constants/Api.cs
@@ -0,0 +1,7 @@
+﻿namespace Logitar.Master.Constants;
+
+internal static class Api
+{
+  public const string Title = "Master API";
+  public static readonly Version Version = new(1, 0, 0);
+}
---
+++ backend/src/Logitar.Master/Controllers/IndexController.cs
@@ -0,0 +1,12 @@
+﻿using Logitar.Master.Models.Index;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logitar.Master.Controllers;
+
+[ApiController]
+[Route("")]
+public class IndexController : ControllerBase
+{
+  [HttpGet]
+  public ActionResult<ApiVersion> Get() => Ok(ApiVersion.Current);
+}
