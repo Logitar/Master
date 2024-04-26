c7e829f04fa267e2b470572b89e2a5f20ba5a46f
Francis Pion <francispion@hotmail.com>
2024-04-26 17:13:42 -0400

    CodeReview.

--- backend/tests/Logitar.Master.IntegrationTests/IntegrationTests.cs
+++ backend/tests/Logitar.Master.IntegrationTests/IntegrationTests.cs
@@ -10,7 +10,6 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System.Text;
 
 namespace Logitar.Master;
 
