ecac279ae416ebbad8387a7b99d0c33da71a0414
Francis Pion <francispion@hotmail.com>
2024-05-07 00:05:06 -0400

    Fixed SMS sending. (#24)

--- backend/src/Logitar.Master.Application/Accounts/IMessageService.cs
+++ backend/src/Logitar.Master.Application/Accounts/IMessageService.cs
@@ -1,4 +1,5 @@
-﻿using Logitar.Portal.Contracts.Messages;
+﻿using Logitar.Master.Contracts.Accounts;
+using Logitar.Portal.Contracts.Messages;
 using Logitar.Portal.Contracts.Users;
 
 namespace Logitar.Master.Application.Accounts;
@@ -7,5 +8,5 @@ public interface IMessageService
 {
   Task<SentMessages> SendAsync(string template, Email email, string? locale = null, Dictionary<string, string>? variables = null, CancellationToken cancellationToken = default);
   Task<SentMessages> SendAsync(string template, Phone phone, string? locale = null, Dictionary<string, string>? variables = null, CancellationToken cancellationToken = default);
-  Task<SentMessages> SendAsync(string template, User user, string? locale = null, Dictionary<string, string>? variables = null, CancellationToken cancellationToken = default);
+  Task<SentMessages> SendAsync(string template, User user, ContactType contactType, string? locale = null, Dictionary<string, string>? variables = null, CancellationToken cancellationToken = default);
 }
